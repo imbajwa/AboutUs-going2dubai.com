37a44e01a6ccaf7304238ef5fdac9c13c73785c0
indrasengupta <indrasengupta111@gmail.com>
2022-11-05 18:47:34 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import AboutUs from "./components/AboutUs";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+
+
+
+function App() {
+  return (
+    <div>
+           <Navbar/>
+
+           <AboutUs/>
+           
+           <Footer/>
+      </div>
+    
+  );
+}
+
+export default App;
